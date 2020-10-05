867623e5a73317ddb37b5c222982b83f3b1e32b7
juliamulko <63002203+juliamulko@users.noreply.github.com>
2020-10-05 18:56:01 +0300

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -16,7 +16,7 @@ module "ecs-cluster-spot" {
   tags             = local.tags
   instance_subnets = module.vpc.private_subnets
   instance_ssh_key = "${local.environment}-app-key" // Your instance key name here
-  security_groups  = "${concat(list("${aws_security_group.ecs_app_1.id}"), "${var.app_cluster_extra_sg}")}"
+  security_groups  = "${concat(list("${aws_security_group.ecs_app.id}"), "${var.app_cluster_extra_sg}")}"
   spotinst_account = var.spotinst_account
   spotinst_token   = var.spotinst_token
 }
