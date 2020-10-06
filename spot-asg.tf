335af067ac98ed4cf752506198e307d853123a50
juliamulko <63002203+juliamulko@users.noreply.github.com>
2020-10-06 08:00:20 +0300

    Update spot-asg.tf

--- spot-asg.tf
+++ spot-asg.tf
@@ -2,56 +2,20 @@ provider "spotinst" {
   account = var.spotinst_account
   token   = var.spotinst_token
 }
-resource "spotinst_ocean_ecs" "ocean-autoscaling-group" {
-  depends_on   = [module.vpc]
-  region       = data.aws_region.current.name
-  name         = var.cluster_name
-  cluster_name = var.cluster_name
-
-  min_size = 0
-
-  subnet_ids = var.instance_subnets
-
-  security_group_ids   = var.security_groups
-  image_id             = data.aws_ssm_parameter.aws_ecs_ami.value
-  iam_instance_profile = aws_iam_instance_profile.ecs-instance-profile.id
-
-  key_pair  = var.instance_ssh_key
-  user_data = data.template_file.ecs_user_data.rendered
-
-  update_policy {
-    should_roll = true
-
-    roll_config {
-      batch_size_percentage = 100
-    }
-  }
-
-  tags {
-    key   = "Name"
-    value = "${aws_ecs_cluster.application.name}-worker"
-  }
-
-  tags {
-    key   = "Environment"
-    value = var.environment
-  }
-
-  tags {
-    key   = "Application"
-    value = "app-${var.environment}"
-  }
-
-  tags {
-    key   = "Monitoring"
-    value = "On"
-  }
-}
 
 data "aws_ssm_parameter" "aws_ecs_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
 }
+resource "aws_ecs_cluster" "application" {
+  name = var.cluster_name
 
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.cluster_name
+    },
+  )
+}
 data "aws_region" "current" {}
 
 data "template_file" "ecs_user_data" {
@@ -136,13 +100,48 @@ module "vpc" {
     "subnet_type" = "private"
   }
 }
-resource "aws_ecs_cluster" "application" {
-  name = var.cluster_name
+resource "spotinst_ocean_ecs" "ocean-autoscaling-group" {
+  depends_on   = [module.vpc]
+  region       = data.aws_region.current.name
+  name         = var.cluster_name
+  cluster_name = var.cluster_name
 
-  tags = merge(
-    var.tags,
-    {
-      "Name" = var.cluster_name
-    },
-  )
+  min_size = 0
+
+  subnet_ids = var.instance_subnets
+
+  security_group_ids   = var.security_groups
+  image_id             = data.aws_ssm_parameter.aws_ecs_ami.value
+  iam_instance_profile = aws_iam_instance_profile.ecs-instance-profile.id
+
+  key_pair  = var.instance_ssh_key
+  user_data = data.template_file.ecs_user_data.rendered
+
+  update_policy {
+    should_roll = true
+
+    roll_config {
+      batch_size_percentage = 100
+    }
+  }
+
+  tags {
+    key   = "Name"
+    value = "${aws_ecs_cluster.application.name}-worker"
+  }
+
+  tags {
+    key   = "Environment"
+    value = var.environment
+  }
+
+  tags {
+    key   = "Application"
+    value = "app-${var.environment}"
+  }
+
+  tags {
+    key   = "Monitoring"
+    value = "On"
+  }
 }
